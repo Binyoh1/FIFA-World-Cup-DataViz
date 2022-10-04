9480e05821b12a27a075341837af19051cf839c4
Binyoh <langhetheo@gmail.com>
2022-10-04 21:50:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -505,7 +505,7 @@ for fig in fig_list:
 st.header("Team Performance in each World Cup")
 # Section Layout
 selected_year = st.selectbox(
-    "Choose the World Cup Edition(s) you wish to view", (year_list)
+    "Choose the World Cup Edition you wish to view", (year_list)
 )
 
 try:
